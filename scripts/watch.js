6a9a0e500fb63f689a1422b680a31eb32fc779e6
Jonathan Dupré <419946+kareniel@users.noreply.github.com>
2022-01-28 10:27:19 -0500

    small visual changes

--- scripts/watch.js
+++ scripts/watch.js
@@ -3,18 +3,20 @@ var budo = require('budo')
 var slashes = require('connect-slashes')
 var build = require('./build')
 
+const PORT = 8080
+
 runBuild()
 
 var b = budo('./src/index.js', {
   live: true,
-  port: 8000,
+  port: PORT,
   dir: path.join(__dirname, '../dist'),
   watchGlob: ['!dist/**', '**/*.{md,pug,styl,yml,png}'],
   staticOptions: {
     extensions: [ 'html' ]
   }
 }).on('connect', e => {
-  console.log('Listening on 8000')
+  console.log('Serving site at http://localhost:' + PORT)
 }).on('watch', e => {
   runBuild()
   b.reload()
